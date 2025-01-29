a2b1a7df9645d9a2a90ed4c22065a4cbcb2c9c8d
JyotirmoyUltragenius <jyotirmoy.banerji@ultragenius.club>
2025-01-29 16:11:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 JIRA_DOMAIN = os.getenv("JIRA_DOMAIN")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
-JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY").strip()
+JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # API Headers
